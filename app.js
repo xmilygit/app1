d19960f3e47da6790e95ced40b3e7d21fa39b286
xmilygit <xmilygame@163.com>
2016-05-13 01:49:08 +0800

    增加了Session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
 var bodyParser = require('body-parser');
 
 var exphbs = require('express-handlebars');
@@ -64,9 +66,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(passport.initialize())
+app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
