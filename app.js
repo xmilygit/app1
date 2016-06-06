c4c2d3aea0e48f6f905a95138a6b4d8c969f7e29
xmilygit <xmilygame@163.com>
2016-06-07 00:46:36 +0800

    微信普通消息回复测试成功

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var session = require('express-session');
 
 var bodyParser = require('body-parser');
 
+var xml2js=require('xml2js');
+
 var exphbs = require('express-handlebars');
 
 var mongoose = require('mongoose');
@@ -70,6 +72,8 @@ app.use(cookieParser());
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(xmlBodyParser);
+
 app.use(passport.initialize())
 app.use(passport.session());
 
@@ -108,5 +112,35 @@ app.use(function (err, req, res, next) {
   });
 });
 
+function xmlBodyParser(req, res, next) {
+    if (req._body) return next();
+    req.body = req.body || {};
+
+    // ignore GET
+    if ('GET' == req.method || 'HEAD' == req.method) return next();
+
+    // check Content-Type
+    if ('text/xml' != req.get('Content-Type')) return next();
+
+    // flag as parsed
+    req._body = true;
+
+    // parse
+    var buf = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk){ buf += chunk });
+    req.on('end', function(){  
+  		var parseString = xml2js.parseString;
+      parseString(buf, function(err, json) {
+        if (err) {
+            err.status = 400;
+            next(err);
+        } else {
+            req.body = json;
+            next();
+        }
+      });
+    });
+};
 
 module.exports = app;
--- routes/weixin.js
+++ routes/weixin.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 var weixin = require('weixin-api');
 var crypto = require('crypto');
-
+var xml2js = require('xml2js');
 weixin.token = 'xmilyhh';
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -18,7 +18,44 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-  res.status(200).send('asdfasdf');
+  //console.log(req.body);
+  //console.log(req.body.xml.Content[0]);
+  var msg=req.body.xml;
+  console.log(msg.Content[0]);
+  console.log(msg.ToUserName[0]);
+  console.log(msg.FromUserName[0]);
+  var time = Math.round(new Date().getTime() / 1000);
+  var output="";
+  switch(msg.Content[0])
+  {
+    case "text":
+    output = 
+	"<xml>" + 
+		 "<ToUserName><![CDATA[" + msg.FromUserName[0] + "]]></ToUserName>" + 
+		 "<FromUserName><![CDATA[" + msg.ToUserName[0] + "]]></FromUserName>" + 
+		 "<CreateTime>" + time + "</CreateTime>" + 
+		 "<MsgType><![CDATA[text]]></MsgType>" + 
+		 "<Content><![CDATA[<a href='www.baidu.com'>adfasdf</a>]]></Content>" + 
+	"</xml>";
+    break;
+    case "link":
+    output = 
+	"<xml>" + 
+		 "<ToUserName><![CDATA[" + msg.FromUserName[0] + "]]></ToUserName>" + 
+		 "<FromUserName><![CDATA[" + msg.ToUserName[0] + "]]></FromUserName>" + 
+		 "<CreateTime>" + time + "</CreateTime>" + 
+		 "<MsgType><![CDATA[link]]></MsgType>" + 
+     "<Title><![CDATA[链接测试]]></Title>"+
+     "<Description><![CDATA[链接描述]]></Description>"+
+     "<Url><![CDATA[www.baidu.com]]></Url>"+
+     "<MsgId>"+msg.MsgId[0]+"</MsgId>"+ 
+	"</xml>";
+    break;
+  }
+	
+ res.set('Content-Type', 'text/xml');
+ res.send(output);
+ //res.end();
 });
 
 
